553692d2bba15cd62754d4e98be9f52d6d8c6ceb
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-22 00:48:53 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -3,27 +3,11 @@ def find_item_by_name_in_collection(name, collection)
   #
   # Consult README for inputs and outputs
 
-total = 0 
-  new_cart = consolidate_cart(cart) 
-  coupon_cart = apply_coupons(new_cart, coupons) 
-  clearance_cart = apply_clearance(coupon_cart) 
-  clearance_cart.each do |item, attribute_hash| 
-    total += (attribute_hash[:price] * attribute_hash[:count])
-  end 
-total = (total * 0.9) if total > 100 total 
 end
 
-def consolidate_cart(cart) 
-  new_cart = {} 
-  cart.each do |items_array| 
-    items_array.each do |item, attribute_hash| 
-      new_cart[item] ||= attribute_hash 
-      new_cart[item][:count] ? new_cart[item][:count] += 1 :   
-      new_cart[item][:count] = 1 
-  end 
-end 
-new_cart 
-end
+def consolidate_cart(cart)
+  
+  
   
   
   # Consult README for inputs and outputs
@@ -31,7 +15,7 @@ end
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
 
-
+end
 
 
   
