d4237550884ea6989dfd94666d8d2c95356d42aa
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-22 00:54:32 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -7,7 +7,15 @@ end
 
 def consolidate_cart(cart)
   
-  
+  new_cart = {} 
+  cart.each do |items_array| 
+    items_array.each do |item, attribute_hash| 
+      new_cart[item] ||= attribute_hash 
+      new_cart[item][:count] ? new_cart[item][:count] += 1 :   
+      new_cart[item][:count] = 1 
+  end 
+end 
+new_cart
   
   
   # Consult README for inputs and outputs
