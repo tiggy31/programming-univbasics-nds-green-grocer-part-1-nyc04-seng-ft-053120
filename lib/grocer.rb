858609dfac36e5246f4b4ae7463b9d5b05367b44
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-22 01:30:53 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -3,7 +3,8 @@ def find_item_by_name_in_collection(name, collection)
   collection.each do |item_info|
   #	    if item_info[:item] == name
   # Consult README for inputs and outputs	      return item_info
-end	    end #if
+end	   
+end #if
   end #each
   nil
   # Implement me first!
