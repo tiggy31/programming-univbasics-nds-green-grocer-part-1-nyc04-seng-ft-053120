773ffc09077f82642ad400ad2310be47cf43c476
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-22 00:45:52 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -3,6 +3,14 @@ def find_item_by_name_in_collection(name, collection)
   #
   # Consult README for inputs and outputs
 
+total = 0 
+  new_cart = consolidate_cart(cart) 
+  coupon_cart = apply_coupons(new_cart, coupons) 
+  clearance_cart = apply_clearance(coupon_cart) 
+  clearance_cart.each do |item, attribute_hash| 
+    total += (attribute_hash[:price] * attribute_hash[:count])
+  end 
+total = (total * 0.9) if total > 100 total 
 end
 
 def consolidate_cart(cart) 
