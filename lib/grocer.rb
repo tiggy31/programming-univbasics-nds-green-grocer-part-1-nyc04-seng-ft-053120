4ac1720f1a4b2fb6807302edbe0d2cdef2ca1429
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-22 01:33:05 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -12,7 +12,8 @@ def consolidate_cart(cart)
   cart.each do |cart_item|
     item_name = cart_item[:item]
     item_info = find_item_by_name_in_collection(item_name, new_cart)
-    
+    if item_info
+      
   
   
 end
